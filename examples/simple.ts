eeef247942a1d995cae2570ce98e04d3b0729f1b
Bryan Kendall <bkend@google.com>
2022-07-07 21:13:11 -0500

    fix example

--- examples/simple.ts
+++ examples/simple.ts
@@ -7,7 +7,7 @@ experiment.use(async () => {
 });
 // try: the candidate. value will be reported in `.publish`.
 experiment.try(async () => {
-  return await new Promise((resolve) => setTimeout(resolve, 10));
+  return await new Promise((resolve) => setTimeout(() => resolve(true), 10));
 });
 // run: run the experiment.
 experiment.run().then((result) => {
